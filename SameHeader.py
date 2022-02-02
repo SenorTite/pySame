2871f21420d06a650853be9c7afac97b933709fd
SenorTite <93595806+SenorTite@users.noreply.github.com>
2022-02-02 20:07:41 +0200

    Add files via upload

--- SameHeader.py
+++ SameHeader.py
@@ -2,8 +2,16 @@ import numpy as np
 from scipy.io import wavfile
 from datetime import datetime as dt
 from enum import Enum
-# import matplotlib.pyplot as plt
+import xml.etree.ElementTree as et
+import json
 
+########### LOAD ##############
+
+events_root = et.parse('events.xml').getroot()
+with open('originatorcodes.json') as f:
+    org_codes = json.load(f)
+
+###############################
 
 # SAME event categories
 class SAMEEventCat(Enum):
@@ -76,11 +84,51 @@ def write_audio_file(audio, date_time):
     scaled_audio = np.int16(audio * 32767)
     wavfile.write(date_time.strftime("SAME %Y%m%d %H%M%S.wav"), 44100, scaled_audio)
 
+###########################################
 
-# Test code
-lcds = ["000000"]
-now = dt.now()
-sm_bin, sm_text = create_header("PEP", emergency_action_notification, lcds, "0001", now, "EASSIMPC")
-print(sm_text)
-aud = binary_to_signal(sm_bin, 44100)
-write_audio_file(aud, now)
+def check_1(str): ###### FIX - Seperate data from code
+    s = str.upper()
+    if s == 'PEP' or s == 'CIV' or s == 'WXR' or s == 'EAS':
+        return s, 0, s + ' - ' + org_codes[s]
+    if s == 'EAN':
+        return s, 1, 'Originator code EAN - EAN Network is no longer in use'
+    return s, 2, '\"' + s + '\" is not a valid originator code'
+
+def check_2(str):
+    if len(str) == 3:
+        s = str.upper()
+        elem = events_root.find('*[@code=\''+s+'\']')
+        if elem is None:
+            return s, 2, '\"' + s + '\" is not a valid event code'
+        if elem.attrib['us'] == 'NI':
+            return s, 1, 'Event \"'+ elem.text + '\" (' + s + ') isn\'t being used by any US agency'
+        return s, 0, s + ' - ' + elem.text
+    # s = str.lower() ##### FIX - make not case sensetive
+    s = str
+    elem = events_root.find('*[.=\'' + s + '\']')
+    if elem is None:
+        return s, 2, 'There is no event called \"' + s + '\" (watch for typos, or use code instead of name)'
+    val = elem.attrib['code']
+    if elem.attrib['us'] == 'NI':
+        return val, 1, 'Event \"' + s + '\" (' + val + ') isn\'t being used by any US agency'
+    return val, 0, val + ' - ' + s
+
+
+###########################################
+
+def test_code():
+    lcds = ["000000"]
+    now = dt.now()
+    sm_bin, sm_text = create_header("PEP", emergency_action_notification, lcds, "0001", now, "EASSIMPC")
+    print(sm_text)
+    aud = binary_to_signal(sm_bin, 44100)
+    write_audio_file(aud, now)
+
+while 1:
+    rtrn, res, msg = check_1(input())
+    if res == 0:
+        print('Chose value: ' + msg)
+    elif res == 1:
+        print('WARNING: ' + msg)
+    elif res == 2:
+        print('ERROR: ' + msg)

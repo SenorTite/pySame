96b3825bec57b652754a4e5d7812cd53173f757b
SenorTite <93595806+SenorTite@users.noreply.github.com>
2022-02-22 17:53:00 +0200

    Add files via upload

--- SameHeader.py
+++ SameHeader.py
@@ -8,8 +8,11 @@ import json
 ########### LOAD ##############
 
 events_root = et.parse('events.xml').getroot()
-with open('originatorcodes.json') as f:
-    org_codes = json.load(f)
+orgcds_root = et.parse('originatorcodes.xml').getroot()
+fips_root = et.parse('fips.xml').getroot()
+with open('uspsAbs.json') as f:
+    state_abvs = json.load(f)
+
 
 ###############################
 
@@ -86,32 +89,104 @@ def write_audio_file(audio, date_time):
 
 ###########################################
 
-def check_1(str): ###### FIX - Seperate data from code
-    s = str.upper()
-    if s == 'PEP' or s == 'CIV' or s == 'WXR' or s == 'EAS':
-        return s, 0, s + ' - ' + org_codes[s]
-    if s == 'EAN':
-        return s, 1, 'Originator code EAN - EAN Network is no longer in use'
-    return s, 2, '\"' + s + '\" is not a valid originator code'
+def check_1(stri):
+    s = stri.upper()
+    fnd = orgcds_root.find(s)
+    if fnd is None:
+        return s, 2, s + ' invalid originator code'
+    if len(fnd.attrib) != 0:
+        return s, 1, 'Originator code ' + s + ' - ' + fnd.text + ' is no longer in use'
+    return s, 0, s + ' - ' + fnd.text
 
-def check_2(str):
-    if len(str) == 3:
-        s = str.upper()
+def check_2(stri):
+    if len(stri) == 3:
+        s = stri.upper()
         elem = events_root.find('*[@code=\''+s+'\']')
         if elem is None:
-            return s, 2, '\"' + s + '\" is not a valid event code'
+            return s, 2, '\"' + s + '\" invalid event code'
         if elem.attrib['us'] == 'NI':
-            return s, 1, 'Event \"'+ elem.text + '\" (' + s + ') isn\'t being used by any US agency'
+            return s, 1, 'Event ' + s + ' - ' + elem.text + ' is not in use'
         return s, 0, s + ' - ' + elem.text
-    # s = str.lower() ##### FIX - make not case sensetive
-    s = str
-    elem = events_root.find('*[.=\'' + s + '\']')
-    if elem is None:
-        return s, 2, 'There is no event called \"' + s + '\" (watch for typos, or use code instead of name)'
-    val = elem.attrib['code']
-    if elem.attrib['us'] == 'NI':
-        return val, 1, 'Event \"' + s + '\" (' + val + ') isn\'t being used by any US agency'
-    return val, 0, val + ' - ' + s
+    s = stri.lower()
+    for elem in events_root:
+        if elem.text.lower() == s:
+            val = elem.attrib['code']
+            if elem.attrib['us'] == 'NI':
+                return val, 1, 'Event ' + val + ' - ' + elem.text + ' is not in use'
+            return val, 0, elem.attrib['code'] + ' - ' + elem.text
+    return stri, 2, 'No event called \"' + stri + '\"'
+
+def check_3(stri):
+    lcds = [x.strip() for x in stri.split('.')]
+    for i in range(len(lcds)):
+        if lcds[i].strip() == '':
+            lcds.pop(i)
+    rtn = []
+    for location in lcds:
+        foo = [x.strip() for x in location.split(',')]
+        if len(foo) > 2:
+            return stri, 2, 'Doesn\'t understand \"' + location + '\"'
+        elif len(foo) == 1:
+            state = foo[0]
+            if len(state) == 2:
+                state = state.upper()
+                if state not in state_abvs.keys():
+                    return stri, 2, state + ' is not a state'
+                rtn.append('0' + fips_root.find('*[@name=\'' + state_abvs[state] + '\']').attrib['id'] + '000')
+            else:
+                state_l = state.lower()
+                found_flag = False
+                for elem in fips_root:
+                    if elem.attrib['name'].lower() == state_l:
+                        # PRONE TO ERROR
+                        rtn.append('0' + elem.attrib['id'] + '000')
+                        found_flag = True
+                        break
+                if not found_flag:
+                    return stri, 2, state_l + ' is not a state'
+        elif len(foo) == 2:
+            state = foo[1]
+            county = foo[0]
+            if len(state) == 2:
+                state = state.upper()
+                if state not in state_abvs.keys():
+                    return stri, 2, state + ' is not a state'
+
+                chosen_state_element = fips_root.find('*[@name=\'' + state_abvs[state] + '\']')
+                found_county_flag = False
+                for elem_county in chosen_state_element:
+                    if elem_county.text.lower() == county.lower():
+                        rtn.append('0' + chosen_state_element.attrib['id'] + elem_county.attrib['id'])
+                        found_county_flag = True
+                        break
+                if not found_county_flag:
+                    return stri, 2, 'County ' + county + ' not found in ' + chosen_state_element.attrib['name']
+            else:
+                state_l = state.lower()
+                chosen_state_element = None
+                found_state_flag = False
+                for elem_state in fips_root:
+                    if elem_state.attrib['name'].lower() == state_l:
+                        chosen_state_element = elem_state
+                        found_state_flag = True
+                        break
+                if not found_state_flag:
+                    return stri, 2, state + ' is not a state'
+                found_county_flag = False
+                for elem_county in chosen_state_element:
+                    if elem_county.text.lower() == county.lower():
+                        rtn.append('0' + chosen_state_element.attrib['id'] + elem_county.attrib['id'])
+                        found_county_flag = True
+                        break
+                if not found_county_flag:
+                    return stri, 2, 'County ' + county + ' not found in ' + chosen_state_element.attrib['name']
+    outmsg = ''
+    for bar in rtn:
+        outmsg += bar + ' '
+    if len(lcds) > 31:
+        return stri, 2, + str(len(lcds)) + ' location codes entered (maximum is 31)'
+    return rtn, 0, outmsg
+
 
 
 ###########################################
@@ -128,9 +203,9 @@ while 1:
     inp = input()
     if inp == 'q':
         break
-    rtrn, res, msg = check_1(inp)
+    rtrn, res, msg = check_3(inp)
     if res == 0:
-        print('Chose value: ' + msg)
+        print('Selected value: ' + msg)
     elif res == 1:
         print('WARNING: ' + msg)
     elif res == 2:
